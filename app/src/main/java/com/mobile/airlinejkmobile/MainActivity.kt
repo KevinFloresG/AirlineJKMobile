b7a4f451649ca52020764a4d488e562f3ace4ecc
KevinFloresG <kevinfg1413@gmail.com>
2021-06-12 11:30:10 -0600

    Base of the project

--- app/src/main/java/com/mobile/airlinejkmobile/MainActivity.kt
+++ app/src/main/java/com/mobile/airlinejkmobile/MainActivity.kt
@@ -2,10 +2,21 @@ package com.mobile.airlinejkmobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main_content.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        val toggle = ActionBarDrawerToggle(this, drawer_layout ,toolbar, R.string.open, R.string.close)
+        toggle.isDrawerIndicatorEnabled = true
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_menu.setNavigationItemSelectedListener(this)
     }
 }
