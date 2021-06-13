520dfd40809a7e55d1795d3efb87b8e9ed250f47
Java64x <55204353+Java64x@users.noreply.github.com>
2021-06-12 22:02:39 -0600

    User.kt + Model.kt

---
+++ app/src/main/java/bLogic/User.kt
@@ -0,0 +1,37 @@
+package bLogic
+
+import java.io.Serializable
+
+class User : Serializable{
+
+    var username:String = ""
+    var password:String = ""
+    var name:String = ""
+    var lastName:String = ""
+    var lastName2:String = ""
+    var email:String = ""
+    var dateOfBirth:String = "" 
+    var address:String = ""
+    var workphone:String = ""
+    var cellphone:String = ""
+    var isAdmin:Int = 0
+    
+    internal constructor(username:String, password:String, name:String, lastName:String, lastName2:String,
+                         email:String, dateOfBirth:String, address:String, workphone:String, cellphone:String, isAdmin:Int){
+
+        this.username = username
+        this.password = password
+        this.name = name
+        this.lastName = lastName
+        this.lastName2 = lastName2
+        this.email = email
+        this.dateOfBirth = dateOfBirth
+        this.address = address
+        this.workphone = workphone
+        this.cellphone = cellphone
+        this.isAdmin = isAdmin
+    }
+
+    internal constructor(){
+    }
+}
