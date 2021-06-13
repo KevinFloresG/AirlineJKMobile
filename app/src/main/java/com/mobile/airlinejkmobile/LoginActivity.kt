2101b5482969d888ebef0919fbad984f07dc7220
Java64x <55204353+Java64x@users.noreply.github.com>
2021-06-12 19:36:30 -0600

    Sign In en progreso

--- app/src/main/java/com/mobile/airlinejkmobile/LoginActivity.kt
+++ app/src/main/java/com/mobile/airlinejkmobile/LoginActivity.kt
@@ -1,11 +1,28 @@
 package com.mobile.airlinejkmobile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.mobile.airlinejkmobile.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val btn = binding.registerBtn
+
+        btn.setOnClickListener(View.OnClickListener {
+            startActivity(
+                Intent(
+                    this@LoginActivity,
+                    SignInActivity::class.java
+                )
+            )
+        })
     }
 }
